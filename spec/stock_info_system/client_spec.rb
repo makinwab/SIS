41248c42229dc31e9dbc6ffd59fd6c1b42064aef
Makinwa Bukola <bukola.makinwa@andela.com>
2018-03-18 18:10:39 +0100

    add spec for client initialization

--- spec/stock_info_system/client_spec.rb
+++ spec/stock_info_system/client_spec.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe StockInfoSystem::Client do
+  subject { StockInfoSystem::Client.new(Fixture::PARSED_USER_INPUT) }
+
+  it "makes available dataset" do
+    expect(subject).to respond_to(:dataset)
+  end
 end
